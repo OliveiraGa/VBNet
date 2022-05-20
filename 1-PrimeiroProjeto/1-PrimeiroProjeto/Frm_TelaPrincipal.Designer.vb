10d3915681f50e2278b2bc622c0c8e6f0d612f6b
Gabriel Oliveira <gabriel.oliveira@alfatest.com.br>
2022-05-20 11:47:09 -0300

    VB Net First Projects

---
+++ 1-PrimeiroProjeto/1-PrimeiroProjeto/Frm_TelaPrincipal.Designer.vb
@@ -0,0 +1,51 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Frm_TelaPrincipal
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Btn_Principal = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Btn_Principal
+        '
+        Me.Btn_Principal.Location = New System.Drawing.Point(120, 64)
+        Me.Btn_Principal.Name = "Btn_Principal"
+        Me.Btn_Principal.Size = New System.Drawing.Size(75, 23)
+        Me.Btn_Principal.TabIndex = 0
+        Me.Btn_Principal.Text = "Teste 1"
+        Me.Btn_Principal.UseVisualStyleBackColor = True
+        '
+        'Frm_TelaPrincipal
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(322, 203)
+        Me.Controls.Add(Me.Btn_Principal)
+        Me.Name = "Frm_TelaPrincipal"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "1 - Projeto"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Btn_Principal As Button
+End Class
---
+++ 1-PrimeiroProjeto/1-PrimeiroProjeto/Frm_TelaPrincipal.vb
@@ -0,0 +1,5 @@
+﻿Public Class Frm_TelaPrincipal
+    Private Sub Btn_Principal_Click(sender As Object, e As EventArgs) Handles Btn_Principal.Click
+        MsgBox("Teste", MsgBoxStyle.Critical, "Mensagem")
+    End Sub
+End Class
