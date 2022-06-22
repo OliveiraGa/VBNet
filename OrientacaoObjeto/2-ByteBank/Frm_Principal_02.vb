3ae37884457c606ba9e75fa3bd800f204d2698ac
Gabriel Oliveira <augusto.gabrielmorais@gmail.com>
2022-06-22 18:00:35 -0300

    Orientação VB

--- OrientacaoObjeto/2-ByteBank/Frm_Principal_02.vb
+++ OrientacaoObjeto/2-ByteBank/Frm_Principal_02.vb
@@ -11,23 +11,19 @@
 
         ' Add any initialization after the InitializeComponent() call.
 
-        ContaGabriela.Titular = New ByteBank.Cliente()
-        ContaGabriela.Titular.Nome = "Gabriela"
-        ContaGabriela.Titular.CPF = 123456789
+        ContaGabriela.Titular = New ByteBank.Cliente("Gabriela", 123456789)
         ContaGabriela.Titular.Estado = "Rio de Janeiro"
         ContaGabriela.Agencia = 863
         ContaGabriela.Conta = 863141
         lblBemVindo.Text = $"Bem vindo(a) {ContaGabriela.Titular.Nome} Agência: {ContaGabriela.Agencia} C/C: {ContaGabriela.Conta}"
-        txtSaldoAtual.Text = ContaGabriela.GetSaldo()
+        txtSaldoAtual.Text = ContaGabriela.Saldo
 
-        ContaBruno.Titular = New ByteBank.Cliente()
-        ContaBruno.Titular.Nome = "Bruno"
-        ContaBruno.Titular.CPF = 125789104
+        ContaBruno.Titular = New ByteBank.Cliente("Bruno", 125789104)
         ContaBruno.Titular.Estado = "São Paulo"
         ContaBruno.Agencia = 863
         ContaBruno.Conta = 863142
         lblBemVindoBruno.Text = $"Bem vindo(a) {ContaBruno.Titular.Nome} Agência: {ContaBruno.Agencia} C/C: {ContaBruno.Conta}"
-        Me.txtSaldoAtualBruno.Text = ContaBruno.GetSaldo()
+        Me.txtSaldoAtualBruno.Text = ContaBruno.Saldo
 
     End Sub
 
@@ -37,12 +33,12 @@
 
         Dim result As Boolean = ContaGabriela.Sacar(valorSacar)
         txtResultado.Text = ""
-        txtSaldoAtual.Text = ContaGabriela.GetSaldo()
+        txtSaldoAtual.Text = ContaGabriela.Saldo
 
         If result Then
-            txtSaldoAtual.Text = ContaGabriela.GetSaldo()
+            txtSaldoAtual.Text = ContaGabriela.Saldo
             txtResultado.Text = "Saque realizado com sucesso!"
-            ContaGabriela.Extrato += $"{Now.ToString} Saque de {valorSacar}{Environment.NewLine}Saldo: {ContaGabriela.GetSaldo()}!{Environment.NewLine}"
+            ContaGabriela.Extrato += $"{Now.ToString} Saque de {valorSacar}{Environment.NewLine}Saldo: {ContaGabriela.Saldo}!{Environment.NewLine}"
         Else
             txtResultado.Text = "Saldo insuficiente para realizar saque!"
         End If
@@ -57,9 +53,9 @@
         txtResultado.Text = ""
         Dim valorDeposito As Double = Val(txtValor.Text)
         ContaGabriela.Depositar(valorDeposito)
-        ContaGabriela.Extrato += $"{Now.ToString} Deposito de {valorDeposito} {Environment.NewLine}Saldo: {ContaGabriela.GetSaldo}!{Environment.NewLine}"
+        ContaGabriela.Extrato += $"{Now.ToString} Deposito de {valorDeposito} {Environment.NewLine}Saldo: {ContaGabriela.Saldo}!{Environment.NewLine}"
         txtValor.Text = ""
-        txtSaldoAtual.Text = ContaGabriela.GetSaldo
+        txtSaldoAtual.Text = ContaGabriela.Saldo
         txtExtrato.Text = ContaGabriela.Extrato
         txtResultado.Text = "Depósito realizado com sucesso"
 
@@ -71,9 +67,9 @@
         txtResultadoBruno.Text = ""
         Dim valorDeposito As Double = Val(txtValorBruno.Text)
         ContaBruno.Depositar(valorDeposito)
-        ContaBruno.Extrato += $"{Now.ToString} Deposito de {valorDeposito} {Environment.NewLine}Saldo: {ContaBruno.GetSaldo}!{Environment.NewLine}"
+        ContaBruno.Extrato += $"{Now.ToString} Deposito de {valorDeposito} {Environment.NewLine}Saldo: {ContaBruno.Saldo}!{Environment.NewLine}"
         txtValorBruno.Text = ""
-        txtSaldoAtualBruno.Text = ContaBruno.GetSaldo
+        txtSaldoAtualBruno.Text = ContaBruno.Saldo
         txtExtratoBruno.Text = ContaBruno.Extrato
         txtResultadoBruno.Text = "Depósito realizado com sucesso"
 
@@ -85,12 +81,12 @@
 
         Dim result As Boolean = ContaBruno.Sacar(valorSacar)
         txtResultadoBruno.Text = ""
-        txtSaldoAtualBruno.Text = ContaBruno.GetSaldo
+        txtSaldoAtualBruno.Text = ContaBruno.Saldo
 
         If result Then
-            txtSaldoAtualBruno.Text = ContaBruno.GetSaldo
+            txtSaldoAtualBruno.Text = ContaBruno.Saldo
             txtResultadoBruno.Text = "Saque realizado com sucesso!"
-            ContaBruno.Extrato += $"{Now.ToString} Saque de {valorSacar}{Environment.NewLine}Saldo: {ContaBruno.GetSaldo}!{Environment.NewLine}"
+            ContaBruno.Extrato += $"{Now.ToString} Saque de {valorSacar}{Environment.NewLine}Saldo: {ContaBruno.Saldo}!{Environment.NewLine}"
         Else
             txtResultadoBruno.Text = "Saldo insuficiente para realizar saque!"
         End If
@@ -107,13 +103,13 @@
             txtResultadoBruno.Text = "Não foi possível realizar a transferencia"
         Else
             txtResultadoBruno.Text = "Tranferencia realizada com sucesso"
-            txtSaldoAtualBruno.Text = ContaBruno.GetSaldo
-            ContaBruno.Extrato += $"Tranferencia de {valor}{Environment.NewLine}Saldo: {ContaBruno.GetSaldo}!{Environment.NewLine}"
+            txtSaldoAtualBruno.Text = ContaBruno.Saldo
+            ContaBruno.Extrato += $"Tranferencia de {valor}{Environment.NewLine}Saldo: {ContaBruno.Saldo}!{Environment.NewLine}"
             txtExtratoBruno.Text = ContaBruno.Extrato
 
 
-            txtSaldoAtual.Text = ContaGabriela.GetSaldo
-            ContaGabriela.Extrato += $"Tranferencia recebida de {valor}{Environment.NewLine}Saldo: {ContaGabriela.GetSaldo}!{Environment.NewLine}"
+            txtSaldoAtual.Text = ContaGabriela.Saldo
+            ContaGabriela.Extrato += $"Tranferencia recebida de {valor}{Environment.NewLine}Saldo: {ContaGabriela.Saldo}!{Environment.NewLine}"
             txtExtrato.Text = ContaGabriela.Extrato
 
         End If
@@ -128,13 +124,13 @@
             txtResultado.Text = "Não foi possível realizar a transferencia"
         Else
             txtResultado.Text = "Tranferencia realizada com sucesso"
-            txtSaldoAtual.Text = ContaGabriela.GetSaldo
-            ContaGabriela.Extrato += $"Tranferencia de {valor}{Environment.NewLine}Saldo: {ContaGabriela.GetSaldo}!{Environment.NewLine}"
+            txtSaldoAtual.Text = ContaGabriela.Saldo
+            ContaGabriela.Extrato += $"Tranferencia de {valor}{Environment.NewLine}Saldo: {ContaGabriela.Saldo}!{Environment.NewLine}"
             txtExtrato.Text = ContaGabriela.Extrato
 
 
-            txtSaldoAtualBruno.Text = ContaBruno.GetSaldo
-            ContaBruno.Extrato += $"Tranferencia recebida de {valor}{Environment.NewLine}Saldo: {ContaBruno.GetSaldo}!{Environment.NewLine}"
+            txtSaldoAtualBruno.Text = ContaBruno.Saldo
+            ContaBruno.Extrato += $"Tranferencia recebida de {valor}{Environment.NewLine}Saldo: {ContaBruno.Saldo}!{Environment.NewLine}"
             txtExtratoBruno.Text = ContaBruno.Extrato
 
         End If
--- OrientacaoObjeto/2-ByteBank/Models/ContaCorrente.vb
+++ OrientacaoObjeto/2-ByteBank/Models/ContaCorrente.vb
@@ -1,23 +1,75 @@
 ﻿Imports _2_ByteBank.ByteBank
 
 Public Class ContaCorrente
-    Public Titular As Cliente
-    Public Agencia As Integer
-    Public Conta As Integer
-    Public Extrato As String = ""
 
-    Private Saldo As Double = 100
-    Public Function GetSaldo() As Double
-        Return Saldo
-    End Function
+#Region "PROPRIEDADES"
+
+    Private m_Titular As Cliente
+    Public Property Titular As Cliente
+        Get
+            Return m_Titular
+        End Get
+        Set(value As Cliente)
+            m_Titular = value
+        End Set
+    End Property
 
 
+    Private m_Agencia As Integer
+    Public Property Agencia() As Integer
+        Get
+            Return m_Agencia
+        End Get
+        Set(ByVal value As Integer)
+            m_Agencia = value
+        End Set
+    End Property
+
+
+    Private m_Conta As Integer
+    Public Property Conta() As Integer
+        Get
+            Return m_Conta
+        End Get
+        Set(ByVal value As Integer)
+            m_Conta = value
+        End Set
+    End Property
+
+    Private m_Extrato As String
+    Public Property Extrato() As String
+        Get
+            Return m_Extrato
+        End Get
+        Set(ByVal value As String)
+            m_Extrato = value
+        End Set
+    End Property
+
+    Private m_Saldo As Double = 100
+    Public Property Saldo As Double
+        Get
+            Return m_Saldo
+        End Get
+        Set(value As Double)
+            If value < 0 Then
+                m_Saldo = 0
+            Else
+                m_Saldo = value
+            End If
+        End Set
+
+    End Property
+
+#End Region
+
+#Region "METODOS"
 
     Public Function Sacar(ByVal ValorSacar As Double) As Boolean
         Sacar = False
 
-        If Saldo >= ValorSacar Then
-            Saldo -= ValorSacar
+        If m_Saldo >= ValorSacar Then
+            m_Saldo -= ValorSacar
             Sacar = True
         End If
 
@@ -25,7 +77,7 @@ Public Class ContaCorrente
     End Function
 
     Public Sub Depositar(ByVal valorDeposito As Double)
-        Saldo += valorDeposito
+        m_Saldo += valorDeposito
     End Sub
 
     Public Function Transferencia(ByVal valorTransferencia As Double, ByRef ContaDestino As ContaCorrente) As Boolean
@@ -38,5 +90,10 @@ Public Class ContaCorrente
 
         Return Transferencia
     End Function
+#End Region
+
+#Region "FUNCOES ESPECIAIS"
+
+#End Region
 
 End Class
